2b318eb43c82694454514ba55c5a90378310cd7e
jared-jackson <lanejackson4@hotmail.com>
2017-09-10 18:11:26 -0500

    Volution-node code

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var cookieParser = require('cookie-parser');
+var path = require('path');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+var http = require('http').Server(app);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use('/', index);
-app.use('/users', users);
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    var normalized_path = path.normalize(__dirname + '/public/index.html');
+    res.sendFile(normalized_path);
+});
+
+
+
+// app.use(function(req, res, next) {
+//     req.db = db;
+//     next();
+// });
+
+http.listen(5000, function() {
+    console.log('HTTP service started.');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
 });
+
+//Keep Program from terminating on exception being thrown
+process.on('uncaughtException', function (err) {
+    console.log(err); //TODO log exceptions being thrown better. This is NOT how you're supposed to do it.
+});
+
 
 module.exports = app;
